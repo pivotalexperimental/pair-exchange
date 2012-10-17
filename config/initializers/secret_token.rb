a1896644e94e2095331bab1377dbac938f7af382
Ryan Spore & Wiley Kestner <pair+rspore+wkestner@pivotallabs.com>
2012-10-16 19:01:50 -0700

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PairExchange::Application.config.secret_token = '28ef392a0fe4a6a84d435468fa466419bebd531a722fed3f1a22b45013b0a824fc98fd68098c0dcb3a1dd51541e53d0021d7f34bd387855c8910fce695aa6df7'
