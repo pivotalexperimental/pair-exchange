f7dc07e70e19e0dea77ea0e0a43e2b299a52b476
David Varvel & Max Brunsfeld <pair+dvarvel+max@pivotallabs.com>
2012-11-13 19:04:17 -0800

    [#38314365] add current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def current_user
+    unless session[:name]
+      @current_user = session[:name]
+    end
+
+    @current_user
+  end
 end
